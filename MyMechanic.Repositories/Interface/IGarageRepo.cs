1f2261f2b6b0afd3aef2a1d3603c6a4ab97ecda6
divyesh kanpariya <divyeshkanpariya888@gmail.com>
2024-02-15 13:58:03 +0530

    All files

---
+++ MyMechanic.Repositories/Interface/IGarageRepo.cs
@@ -0,0 +1,28 @@
+﻿using MyMechanic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMechanic.Repositories.Interface
+{
+    public interface IGarageRepo
+    {
+        public List<GarageModel> GetGarageDetails(long MechanicId);
+
+        public void AddEditGarage(NewGarageModel data);
+
+        public void DeleteGarage(long GarageId);
+
+        public void UploadGaragePhotos(NewGaragePhotosModel model, long GarageId, long UserId);
+
+        public List<GaragePhotosModel> GetGaragePhotos(long GarageId, long UserId);
+
+        public string DeleteGaragePhoto(long GarageId, long GaragePhotoId);
+
+        public List<GarageModel> GetGarageDataForCustomer();
+
+        public GarageModel GetGarageDataByGarageId(long GarageId);
+    }
+}
---
+++ MyMechanicBackend/Controllers/GarageController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyMechanic.Repositories.Interface;
+using MyMechanic.ViewModels;
+
+namespace MyMechanicBackend.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GarageController : ControllerBase
+    {
+        private readonly IGarageRepo _GarageRepo;
+
+        public GarageController(IGarageRepo GarageRepo)
+        {
+            _GarageRepo = GarageRepo;
+        }
+        [HttpGet("GetGarageDetails")]
+        public IActionResult GetGarageDetails(string MechanicId)
+        {
+            
+            return Ok(_GarageRepo.GetGarageDetails(Convert.ToInt64(MechanicId)));
+        }
+
+        [HttpPost("AddEditGarage")]
+        public IActionResult AddEditGarage(NewGarageModel data)
+        {
+            
+            _GarageRepo.AddEditGarage(data);
+            return Ok();
+        }
+
+        [HttpDelete("DeleteGarage")]
+        public IActionResult DeleteGarage(long GarageId)
+        {
+            _GarageRepo.DeleteGarage(Convert.ToInt64(GarageId));
+            return Ok();
+        }
+
+        [HttpPost("AddGaragePhotos")]
+        public IActionResult AddGaragePhotos([FromForm] NewGaragePhotosModel model, long GarageId, long UserId)
+        {
+            if (model?.Files == null || model.Files.Count == 0)
+            {
+                return BadRequest("No files were uploaded.");
+            }
+
+            // Process each uploaded file
+            _GarageRepo.UploadGaragePhotos(model,GarageId,UserId);
+
+            return Ok();
+        }
+
+        
+        [HttpGet("GetGaragePhotos")]
+        public IActionResult GetGaragePhotos(long GarageId, long UserId)
+        {
+            
+            return Ok(_GarageRepo.GetGaragePhotos(GarageId, UserId));
+        }
+
+        [HttpDelete("DeleteGaragePhoto")]
+        public IActionResult DeleteGaragePhoto(long GarageId, long GaragePhotoId)
+        {
+            
+            String status = _GarageRepo.DeleteGaragePhoto(GarageId, GaragePhotoId);
+            if (status == "Image Does not Exiest") return BadRequest(status);
+            else return Ok();
+        }
+
+        [HttpGet("GetGarageDataForCustomer")]
+        public IActionResult GetGarageDataForCustomer()
+        {
+            var x = _GarageRepo.GetGarageDataForCustomer();
+            return Ok(x);
+        }
+        [HttpGet("GetGarageDataByGarageId")]
+        public IActionResult GetGarageDataByGarageId(long GarageId)
+        {
+            return Ok(_GarageRepo.GetGarageDataByGarageId(GarageId));
+        }
+    }
+}
